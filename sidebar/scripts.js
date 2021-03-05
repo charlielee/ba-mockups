cc9f54d504744c9f82bd3f76fa20d8798dc8f20b
Charlie Lee <dev@charlielee.uk>
2021-03-05 21:13:50 +0000

    Add sidebar mockup

---
+++ sidebar/scripts.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", function () {
+  /**
+   * Activate tabs
+   */
+  document.querySelector(".tabs").addEventListener("click", function (e) {
+    // Get the active pane
+    let activeTab = document.querySelector(".tabs .active");
+    let activeTabPane = document.querySelector(".tab-pane.active");
+
+    // Switch pane if a tab is found
+    let newTab = e.target;
+    if (newTab.getAttribute("data-toggle") === "tab") {
+      // Get the new tab pane
+      let newTabPaneTarget = newTab.getAttribute("href");
+      let newTabPane = document.querySelector(newTabPaneTarget);
+
+      activeTab.classList.remove("active");
+      activeTabPane.classList.remove("active");
+      newTab.classList.add("active", true);
+      newTabPane.classList.add("active", true);
+    }
+  });
+
+  /**
+   * Activate modals
+   */
+  document.querySelector("#openSettingsModalBtn").addEventListener("click", function () {
+    alert("Some cool modal full of settings will one day appear here.")
+  });
+});
